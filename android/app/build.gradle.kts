431ec0a7989b1462e00047a8d15d873693ef88d8
Chad Jensen <chadbjensen@gmail.com>
2026-08-31 14:34:52 -0600

    chore(android): prepare 1.0.23 release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.mobileegress.agent"
         minSdk = 29
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.15"
+        versionCode = 11
+        versionName = "1.0.23"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
